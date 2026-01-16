5eb97a95dc74e3f7a6488dea348d9588423380bd
Kevin Bouman <kevin@MacBook-Pro-van-Kevin.local>
2026-01-17 00:09:36 +0100

    Restore all sensors and prevent unavailable state

--- custom_components/autodarts/sensor.py
+++ custom_components/autodarts/sensor.py
@@ -8,9 +8,16 @@ from .const import DOMAIN
 
 
 SENSORS = {
+    "dart1": "Dart 1",
+    "dart1_value": "Dart 1 Value",
+    "dart2": "Dart 2",
+    "dart2_value": "Dart 2 Value",
+    "dart3": "Dart 3",
+    "dart3_value": "Dart 3 Value",
     "throw_summary": "Throw Summary",
     "turn_total": "Turn Total",
     "remaining": "Remaining",
+    "leg_result": "Leg Result",
 }
 
 
@@ -42,7 +49,13 @@ class AutodartsSensor(CoordinatorEntity, SensorEntity):
 
     @property
     def native_value(self):
-        if not self.coordinator.data:
-            return None
-        return self.coordinator.data.get(self._key)
+        data = self.coordinator.data or {}
+
+        value = data.get(self._key)
+
+        # Home Assistant wil GEEN None
+        if value is None:
+            return ""
+
+        return value
 
