33e9da0655e0c10594c4aadf115deb4bdb83e0c7
Kevin Bouman <kevin@MacBook-Pro-van-Kevin.local>
2026-01-16 20:08:31 +0100

    Fix config flow handler and domain mismatch

--- custom_components/autodarts/config_flow.py
+++ custom_components/autodarts/config_flow.py
@@ -1,7 +1,11 @@
-from homeassistant import config_entries
+from __future__ import annotations
+
 import voluptuous as vol
 
-from .const import DOMAIN, CONF_HOST, CONF_PORT, DEFAULT_PORT
+from homeassistant import config_entries
+from homeassistant.core import callback
+
+from .const import DOMAIN
 
 
 class AutodartsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -14,13 +18,15 @@ class AutodartsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 data=user_input,
             )
 
+        schema = vol.Schema(
+            {
+                vol.Required("host"): str,
+                vol.Required("port", default=3180): int,
+            }
+        )
+
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema(
-                {
-                    vol.Required(CONF_HOST): str,
-                    vol.Optional(CONF_PORT, default=DEFAULT_PORT): int,
-                }
-            ),
+            data_schema=schema,
         )
 
